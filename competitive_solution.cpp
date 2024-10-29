471eb79af11de9a8f650c6a849e1b28ff5dec21b
Joshua Ruiz <110396328+Joshuasacc@users.noreply.github.com>
2024-10-29 16:24:04 +0800

    Add files via upload

---
+++ competitive_solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+unsigned long long int factorial(long long n) {
+    if (n < 1) return 1;
+    else return n * factorial(n - 1);
+}
+int main() {
+    long long x;
+    cin >> x;
+    string num = to_string(factorial (x));
+    reverse (num.begin(), num.end());
+    string num2;
+
+    if (num.length () >= 4) {
+        for (int i = 0; i < 4; i++) num2 +=num[i];
+    } else for (int i = 0; i < num.length (); i++) num2 +=num[i];
+
+	reverse (num2.begin (), num2.end ());
+    cout << num2;
+    return 0;
+}
